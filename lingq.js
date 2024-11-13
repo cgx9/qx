22ecd74b5705303f2f29b2db1b682b869e8562c2
Chris Chen <guoxing45@gmail.com>
2024-11-13 16:44:39 +0800

    try lingq2

--- lingq.js
+++ lingq.js
@@ -1,7 +1,7 @@
 /*************************************
 
 [rewrite_local]
-^https:\/\/www\.lingq\.com\/api\/v2\/profiles\/ url script-response-body https://raw.githubusercontent.com/cgx9/qx/main/lingq.js
+^https:\/\/www\.lingq\.com\/api\/v2\/apple\/purchase\/ url script-response-body https://raw.githubusercontent.com/cgx9/qx/main/lingq.js
 
 [mitm]
 hostname = www.lingq.com
@@ -11,10 +11,13 @@ hostname = www.lingq.com
 
 var d = JSON.parse($response.body);
 const url = $request.url;
+console.log(d)
 
-
-if(d.results && d.results.length > 0){
-    d.results[0]['role'] = 'premium'
-    d.results[0]['use_classic_mode'] = false
+if(d.prices){
+    //d.results[0]['role'] = 'premium'
+    //d.results[0]['use_classic_mode'] = false
+    d.prices['premiumAuto.ja'] = 0.00
+    d.prices['premiumAuto12.ja'] = 0.00
+    d.prices['premiumAuto12.ja.nt'] = 0.00
 }
 $done({body : JSON.stringify(d)});
