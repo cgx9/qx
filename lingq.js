113caf89b231bbf5daa7f51ccd5f66bf26139df8
Chris Chen <guoxing45@gmail.com>
2024-11-13 20:06:16 +0800

    try lingq7

--- lingq.js
+++ lingq.js
@@ -1,7 +1,7 @@
 /*************************************
 
 [rewrite_local]
-^https:\/\/www\.lingq\.com\/api\/v2\/apple\/purchase\/ url script-response-body https://raw.githubusercontent.com/cgx9/qx/main/lingq.js
+^https:\/\/www\.lingq\.com\/api\/v2\/profiles\/$ url script-response-body https://raw.githubusercontent.com/cgx9/qx/main/lingq.js
 
 [mitm]
 hostname = www.lingq.com
@@ -12,13 +12,15 @@ hostname = www.lingq.com
 var d = JSON.parse($response.body);
 const url = $request.url;
 
-if(d.prices){
-    //d.results[0]['role'] = 'premium'
-    //d.results[0]['use_classic_mode'] = false
-    d.prices['premiumAuto.ja'] = 0.00
-    d.prices['premiumAuto.ja.nt'] = 0.00
-    d.prices['premiumAuto12.ja'] = 0.00
-    d.prices['premiumAuto12.ja.nt'] = 0.00
+//if(d.prices){
+if(d.results && d.results.length>0)
+    d.results[0]['role'] = 'Premium'
+    d.results[0]['use_classic_mode'] = false
+    d.results[0]['is_beta_tester'] = true
+    //d.prices['premiumAuto.ja'] = 0.00
+    //d.prices['premiumAuto.ja.nt'] = 0.00
+    //d.prices['premiumAuto12.ja'] = 0.00
+    //d.prices['premiumAuto12.ja.nt'] = 0.00
 }
 if(d.profiles){
     try{
