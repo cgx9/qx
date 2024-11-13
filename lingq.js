e091d6440380680ee58cabb575f508f99afe55fa
Chris Chen <guoxing45@gmail.com>
2024-11-13 19:20:05 +0800

    try lingq4

--- lingq.js
+++ lingq.js
@@ -1,7 +1,7 @@
 /*************************************
 
 [rewrite_local]
-^https:\/\/www\.lingq\.com\/api\/v2\/apple\/purchase\/ url script-request-body https://raw.githubusercontent.com/cgx9/qx/main/lingq.js
+^https:\/\/www\.lingq\.com\/api\/v2\/apple\/purchase\/ url script-response-body https://raw.githubusercontent.com/cgx9/qx/main/lingq.js
 
 [mitm]
 hostname = www.lingq.com
@@ -9,9 +9,8 @@ hostname = www.lingq.com
 *************************************/
 
 
-var d = JSON.parse($request.body);
+var d = JSON.parse($response.body);
 const url = $request.url;
-console.log(d)
 
 if(d.prices){
     //d.results[0]['role'] = 'premium'
@@ -21,4 +20,14 @@ if(d.prices){
     d.prices['premiumAuto12.ja'] = 0.00
     d.prices['premiumAuto12.ja.nt'] = 0.00
 }
+if(d.profiles){
+    var b = {
+        'role' = 'Premium',
+        "expires_date" : "2099-09-09 09:09:09 Etc/GMT",
+        "purchase_date" : "2023-09-09 09:09:09 Etc/GMT",
+        "is_beta_tester" : false,
+    }
+    d.profiles.push(b)
+}
+console.log(d)
 $done({body : JSON.stringify(d)});
