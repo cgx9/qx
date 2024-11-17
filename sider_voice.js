b9dff71dd61e400545618854e9b77226fada5572
Chris Chen <guoxing45@gmail.com>
2024-11-18 07:56:50 +0800

    sider voice url

--- sider_voice.js
+++ sider_voice.js
@@ -1,7 +1,7 @@
 /*************************************
 
 [rewrite_local]
-^https:\/\/api1\.sider\.ai\/api\/v1\/audio\/voice\/list url script-response-body https://raw.githubusercontent.com/cgx9/qx/main/sider.js
+^https:\/\/api1\.sider\.ai\/api\/v1\/audio\/voice\/list url script-response-body https://raw.githubusercontent.com/cgx9/qx/main/sider_voice.js
 
 [mitm]
 hostname = api1.sider.ai
