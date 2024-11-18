a9a31221d93ecac680d1b58584cfde2ab067ede6
Chris Chen <guoxing45@gmail.com>
2024-11-18 08:34:17 +0800

    sider path

--- sider_voice.js
+++ sider_voice.js
@@ -1,22 +1,24 @@
 /*************************************
 
 [rewrite_local]
-^https:\/\/(api\d*\.sider\.ai|sider\.ai)\/api\/v1\/audio\/voice\/list url script-response-body https://raw.githubusercontent.com/cgx9/qx/main/sider_voice.js
-
+^https:\/\/(api\d*\.sider\.ai|sider\.ai)\/api\/v1\/payment\/trial_check url script-response-body https://raw.githubusercontent.com/cgx9/qx/main/sider_voice.js
 [mitm]
-hostname = api1.sider.ai,sider.ai
+hostname = api1.sider.ai,sider.ai,api.sider.ai
 
 *************************************/
 
 var d = JSON.parse($response.body);
 const url = $request.url;
-
-if (d.data && d.data.list) {
-  d.data.list = d.data.list.map((item) => {
-    item.need_premium = false;
-    return item;
-  });
+if (d.data) {
+  d.data["allow"] = true;
 }
+// if (d.data && d.data.list) {
+//   d.data.list = d.data.list.map((item) => {
+//     item.need_premium = false;
+//     return item;
+//   });
+// }
 
 console.log(url.pathname);
 $done({ body: JSON.stringify(d) });
+//^https:\/\/(api\d*\.sider\.ai|sider\.ai)\/api\/v1\/audio\/voice\/list url script-response-body https://raw.githubusercontent.com/cgx9/qx/main/sider_voice.js
