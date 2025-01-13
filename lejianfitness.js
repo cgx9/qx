2a1ab61ce22582b00013920bfc27ca20fa1f168d
Chris Chen <guoxing45@gmail.com>
2025-01-13 21:50:38 +0800

    lejian

---
+++ lejianfitness.js
@@ -0,0 +1,24 @@
+/*************************************
+[rewrite_local]
+^https:\/\/prod-saas\.ieyoutech\.com\.cn\/user\/login\/\S+$ url script-response-body https://raw.githubusercontent.com/cgx9/qx/main/lejianfitness.js
+
+[mitm]
+hostname = prod-saas.ieyoutech.com.cn
+*************************************/
+let responseBody = $response.body;
+
+let responseJson = JSON.parse(responseBody);
+if(responseJson.result){
+  let res = responseJson.result;
+  res['deleted'] = 0;
+  res['vipAmount'] = 1;
+  res['userType'] = 1;
+  res['membershipLevel'] = 2;
+  res['expiryTime'] = '2026-01-21 20:00:49'
+
+  responseJson.result = res;
+}
+
+responseBody = JSON.stringify(responseJson);
+$done({ body: responseBody });
+
