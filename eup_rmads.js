30e095893e3e9d1a10f2c5510a602012e401388a
Chris Chen <guoxing45@gmail.com>
2025-01-22 14:10:13 +0800

    eup rm ads

--- eup_rmads.js
+++ eup_rmads.js
@@ -8,11 +8,24 @@ hostname = product.eupgroup.net
 *************************************/
 
 var d = JSON.parse($response.body);
-d['Ads'] = null;
-// d['Ads']['top_1_ios'] = null;
-// d['Ads']['top_2_ios'] = null;
-// d['Ads']['top_3_ios'] = null;
-// d['Ads']['sale_ios'] = null;
+// d['Ads'] = null;
+d['Ads']['top_1_ios'] = null;
+d['Ads']['top_2_ios'] = null;
+d['Ads']['top_3_ios'] = null;
+d['Ads']['sale_ios'] = [
+    {
+      "premium" : "preforevermonths",
+      "percent" : "100"
+    },
+    {
+      "premium" : "pre12months",
+      "percent" : "50"
+    },
+    {
+      "premium" : "pre3months",
+      "percent" : "30"
+    }
+  ];
 console.log(JSON.stringify(d));
 $done({ body: JSON.stringify(d) });
 
